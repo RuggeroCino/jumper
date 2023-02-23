538b897803f4fda7da3e9080f79ede416ac297b9
Ruggero Cino <ruggero.cino@gmail.com>
2023-02-23 18:51:43 +0100

    Update logos and readme, add tests to start block

--- src/components/start-block/start-block.tsx
+++ src/components/start-block/start-block.tsx
@@ -1,15 +1,33 @@
 import { RigidBody } from '@react-three/rapier';
+import { Float, Text } from '@react-three/drei';
 import React from 'react';
+import { isMobile } from 'react-device-detect';
+import { boxGeometry, startBlockMaterial } from '../../constants';
 
 export interface IStartBlockProps {};
 
 export const StartBlock: React.FC<IStartBlockProps> = () => {
+    const text = isMobile ? 'Tap screen to start' : 'Press space to start';
+
     return (
-        <RigidBody type="fixed" friction={1} position={[0, 0, 10]} rotation={[0, 0, 0]}>
-            <mesh>
-                <boxGeometry args={[2, 0.25, 2]} />
-                <meshStandardMaterial color="black" metalness={5} />
-            </mesh>
-        </RigidBody>
+        <>
+            <Float floatIntensity={0.5} rotationIntensity={0.25}>
+                <Text
+                    font="./bebas-neue-v9-latin-regular.woff"
+                    scale={2.5}
+                    maxWidth={10}
+                    lineHeight={0.75}
+                    textAlign="left"
+                    position={[-5, 5, 10]}
+                    rotation-y={Math.PI / 2}
+                >
+                    {text}
+                    <meshBasicMaterial toneMapped={false} />
+                </Text>
+            </Float>
+            <RigidBody type="fixed" friction={1} position={[0, 0, 10]} rotation={[0, 0, 0]} scale={[3, 0.5, 3]}>
+                <mesh geometry={boxGeometry} material={startBlockMaterial} />
+            </RigidBody>
+        </>
     );
 };
--- src/constants/materials.ts
+++ src/constants/materials.ts
@@ -3,6 +3,8 @@ import { Color } from 'three';
 
 export const playerMaterial = new THREE.MeshStandardMaterial({ metalness: 0.5, roughness: 0, color: new Color('rgb(60, 147, 255)') });
 
+export const startBlockMaterial = new THREE.MeshStandardMaterial({ metalness: 1, color: 'black' });
+
 export const obstacleMaterial = new THREE.MeshStandardMaterial({
     displacementScale: 0.005,
     displacementBias: -0.005,
